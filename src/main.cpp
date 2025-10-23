a9493cad3d42bb523f302c5161b4491909e74e33
nv3033 <116170942+nv3033@users.noreply.github.com>
2025-10-23 13:07:36 +0300

    1

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+int main() {
+    std::cout << "yummy" << std::endl;
+}
